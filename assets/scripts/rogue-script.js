8b71e8698d31c73bd0af285c3ca5d71d56d8e055
Xris <cp@maxfractal.com>
2024-04-14 01:28:24 -0400

    All scratch code

--- assets/scripts/rogue-script.js
+++ assets/scripts/rogue-script.js
@@ -5,14 +5,32 @@ const scene = {
     create: function () {
         this.helloText = this.add.bitmapText(400,300, "arcade", "Hello Maxfractal").setOrigin(0.5);
         this.helloXris = this.add.bitmapText(400,300, "arcade", " xris fractal").setOrigin(0.5);
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //debug info
+        this.textLoc = this.add.bitmapText(100,100, "arcade", "X = ").setOrigin(0.5);
     },
     update: function() {
-        this.helloText.x += 10;
-        this.helloXris.y += 10;
-
-        if(this.helloText.x > 1000) {
-            this.helloText.x = -200
+        if(this.cursors.left.isDown){
+            this.helloText.x -= 10;
         }
+        if(this.cursors.right.isDown){
+            this.helloText.x += 10;
+        }
+        if(this.cursors.up.isDown){
+            this.helloText.y -= 10;
+        }
+        if(this.cursors.down.isDown){
+            this.helloText.y += 10;
+        }
+
+        //console.log(this.helloText.x);
+        //this.helloXris.y += 10;
+        
+        //this.helloText.x += 10;
+        //if(this.helloText.x > 1000) {
+        //    this.helloText.x = -200
+        //}
 
         if(this.helloXris.y > 630) {
             this.helloXris.y = -100

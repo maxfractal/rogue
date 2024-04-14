d80048593f5a2689b1f66237cacb89143d3bfe4d
Xris <cp@maxfractal.com>
2024-04-14 02:34:32 -0400

    tilemap not working

--- assets/scripts/rogue-script.js
+++ assets/scripts/rogue-script.js
@@ -1,11 +1,49 @@
 const scene = {
     preload: function () {
-        this.load.bitmapFont("arcade", "/assets/font/arcade.png", "/assets/font/arcade.xml");
+        //this.load.bitmapFont("arcade", "/assets/font/arcade.png", "/assets/font/arcade.xml");
+        this.load.spritesheet(
+            'tiles',
+            '/assets/graphics/colored.png',
+            {
+                frameWidth: 16,
+                frameHeight: 16,
+                spacing: 1
+            }
+        )
     },
     create: function () {
-        this.helloText = this.add.bitmapText(400,300, "arcade", "Hello Maxfractal").setOrigin(0.5);
-        this.helloXris = this.add.bitmapText(400,300, "arcade", " xris fractal").setOrigin(0.5);
-        this.cursors = this.input.keyboard.createCursorKeys();
+
+        let level = [
+            [1,1,1,1,1,1,1,1,1,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,1,1,1,1,1,1,1,1,1],
+        ]
+
+        const wall = 554
+        const floor = 0
+        level = level.map(r => r.map(t => t == 1 ? wall : floor))
+
+        //draw the tilemap
+        const tileSize = 16
+        const config = {
+            data: level,
+            tileWidth: tileSize,
+            tileHeight: tileSize,
+        }
+        const map = this.make.tilemap(config);
+        const tileset = map.addTilesetImage('tiles', 'tiles', tileSize, tileSize, 0, 1)
+
+        const ground = map.createStaticLayer(0, tileset, 0, 0)
+
+
+        //this.cursors = this.input.keyboard.createCursorKeys();
     },
 
     update: function() {
@@ -27,11 +65,12 @@ const scene = {
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 80 * 16,
+    height: 50 * 16,
     backgroundColor: "#016",
     parent: "game",
     pixelArt: true,
+    zoom: 2,
     scene: scene,
     physics: {
         default: "arcade",
